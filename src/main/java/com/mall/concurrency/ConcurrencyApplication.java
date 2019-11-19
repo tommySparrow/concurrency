52761ba07378f8c1704af43c182bfa61f87c3de8
jm90_yang <jm90_yang@163.com>
2019-11-19 19:21:20 +0800

    concurrency

---
+++ src/main/java/com/mall/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.mall.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mall/concurrency/example/automic/aqs/FutureTaskExample.java
@@ -0,0 +1,25 @@
+package com.mall.concurrency.example.automic.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+@Slf4j
+public class FutureTaskExample {
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask =  new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("do sth in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+        new Thread(futureTask).start();
+        log.info("do sth in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result:{}",result);
+    }
+}
